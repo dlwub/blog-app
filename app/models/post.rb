e7835779e5f79b1a9e1c8cf0cd7b09b45e439bc3
dlwub <dlwubi@gmail.com>
2023-01-19 17:31:18 +0300

    Install rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,19 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: :post_id
-  has_many :likes, foreign_key: :post_id
+	belongs_to :author, class_name: 'User'
+	has_many :comments, foreign_key: :post_id
+	has_many :likes, foreign_key: :post_id
+	validates :title, presence: true, length: { maximum: 250 }
+	validates :comments_counter, length: { minimum: 0 }
+	validates :likes_counter, length: { minimum: 0 }
 
-  after_save :post_count
-  def recent_comment
-    comments.order(created_at: :desc).limit(5)
-  end
+	after_save :post_count
+	def recent_comment
+		comments.order(created_at: :desc).limit(5)
+	end
 
-  private
+	private
 
-  def post_count
-    author.update(posts_counter: author.posts.all.length)
-  end
+	def post_count
+		author.update(posts_counter: author.posts.all.length)
+	end
 end
