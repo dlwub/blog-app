b1516c85828b94ce2b9483db3b471589ca24e2fa
dlwub <dlwubi@gmail.com>
2023-01-25 09:27:47 +0300

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "Posts controller" do
-    it "is a success" do
-      get "/users/1/posts"
+RSpec.describe 'Posts', type: :request do
+  describe 'Posts controller' do
+    it 'is a success' do
+      get '/users/1/posts'
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
-      get "/users/1/posts"
+      get '/users/1/posts'
       expect(response).to render_template('index')
-      expect(response.body).to include("List of all posts by")
+      expect(response.body).to include('List of all posts by')
     end
 
     it 'is a success' do
-      get "/users/1/posts/1"
+      get '/users/1/posts/1'
       expect(response).to have_http_status(:ok)
     end
 
     it "renders 'show' template" do
-      get "/users/1/posts/1"
+      get '/users/1/posts/1'
       expect(response).to render_template('show')
-      expect(response.body).to include("Post")
+      expect(response.body).to include('Post')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,27 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "Usetrs index" do
-    before(:example) { get users_path } #get(:index)
-    it "is a success" do
+RSpec.describe 'Users', type: :request do
+  describe 'Usetrs index' do
+    before(:example) { get users_path } # get(:index)
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
     it "renders 'index' template" do
       expect(response).to render_template('index')
-      expect(response.body).to include("List of all users")
+      expect(response.body).to include('List of all users')
     end
 
     it 'is a success' do
-      get "/users/1"
+      get '/users/1'
       expect(response).to have_http_status(:ok)
     end
 
     it "renders 'show' template" do
-      get "/users/1"
+      get '/users/1'
       expect(response).to render_template('show')
-      expect(response.body).to include("Tom")
+      expect(response.body).to include('Tom')
     end
-
   end
 end
