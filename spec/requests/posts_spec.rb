cf749624cd87123cda3558b7e8a79e848c05d42e
dlwub <dlwubi@gmail.com>
2023-01-25 09:22:18 +0300

    Add tests for controller actions

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    before(:example) { get user_posts_path }
+  describe "Posts controller" do
     it "is a success" do
+      get "/users/1/posts"
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
+      get "/users/1/posts"
       expect(response).to render_template('index')
+      expect(response.body).to include("List of all posts by")
+    end
+
+    it 'is a success' do
+      get "/users/1/posts/1"
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      get "/users/1/posts/1"
+      expect(response).to render_template('show')
+      expect(response.body).to include("Post")
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
+  describe "Usetrs index" do
     before(:example) { get users_path } #get(:index)
     it "is a success" do
       expect(response).to have_http_status(:ok)
@@ -9,6 +9,18 @@ RSpec.describe "Users", type: :request do
 
     it "renders 'index' template" do
       expect(response).to render_template('index')
+      expect(response.body).to include("List of all users")
+    end
+
+    it 'is a success' do
+      get "/users/1"
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      get "/users/1"
+      expect(response).to render_template('show')
+      expect(response.body).to include("Tom")
     end
 
   end
