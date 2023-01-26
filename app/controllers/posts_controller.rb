0613e974e06cf478cb098d7e0a3afdd109d155a9
dlwub <dlwubi@gmail.com>
2023-01-26 23:19:43 +0300

    Update button link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.all
+    @posts = Post.where(:user_id => @user.id)
   end
 
   def show
     @user = User.find(params[:user_id])
-    @posts = @user.posts.all
+    @posts = Post.where(:user_id => @user.id)
     @post = Post.find(params[:id])
     @comments = Comment.where(post_id: params[:id])
   end
