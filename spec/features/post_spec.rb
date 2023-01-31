f6fb745c35355d656a9e0505d7fca0ef625769d0
dlwub <dlwubi@gmail.com>
2023-01-31 21:07:04 +0300

    Fix linter errors

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe 'Page', type: :feature do
       photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
       bio: 'Teacher from Addis.'
     )
-    @post_1 = Post.create(id: 1, author: @user, title: 'Hello', text: "This is my post number 1")
-    @post_2 = Post.create(id: 2, author: @user, title: 'Hello', text: "This is my post number 2")
-    @post_3 = Post.create(id: 3, author: @user, title: 'Hello', text: "This is my post number 3")
-    @post_4 = Post.create(id: 4, author: @user, title: 'Hello', text: "This is my post number 4")
-    (1..6).each do |i|
+    @post_1 = Post.create(id: 1, author: @user, title: 'Hello', text: 'This is my post number 1')
+    @post_2 = Post.create(id: 2, author: @user, title: 'Hello', text: 'This is my post number 2')
+    @post_3 = Post.create(id: 3, author: @user, title: 'Hello', text: 'This is my post number 3')
+    @post_4 = Post.create(id: 4, author: @user, title: 'Hello', text: 'This is my post number 4')
+    (1..6).each do |_i|
       Comment.create(post: @post_1, author: @user, text: 'Hello Zaid!')
     end
-    (1..5).each do |i|
+    (1..5).each do |_i|
       Like.create(post: @post_1, author: @user)
     end
     visit user_posts_path(@user)
@@ -33,23 +33,23 @@ RSpec.describe 'Page', type: :feature do
       expect(page).to have_content('Hello')
     end
     it "shows the post's body" do
-      expect(page).to have_content("This is my post number 1")
+      expect(page).to have_content('This is my post number 1')
     end
     it 'shows the first comments on a post' do
-      expect(page).to have_selector('br', count:5)
+      expect(page).to have_selector('br', count: 5)
     end
     it 'shows the number of comments the post has' do
-      expect(page).to have_content("Comments: 6")
+      expect(page).to have_content('Comments: 6')
     end
     it 'shows the number of likes the post has' do
-      expect(page).to have_content("Likes: 5")
+      expect(page).to have_content('Likes: 5')
     end
     it 'option for pagination' do
-      expect(page).to have_content("Pagination")
+      expect(page).to have_content('Pagination')
     end
     it 'redirects to user posts show page' do
       click_link 'Post # 1'
-      expect(page).to have_selector('br', count:6)
+      expect(page).to have_selector('br', count: 6)
     end
   end
   describe 'post show page' do
@@ -63,19 +63,19 @@ RSpec.describe 'Page', type: :feature do
       expect(page).to have_content('by Zaid')
     end
     it 'shows number of comments' do
-      expect(page).to have_content("Comments: 6")
+      expect(page).to have_content('Comments: 6')
     end
-    it "shows number of likes" do
-      expect(page).to have_content("Likes: 5")
+    it 'shows number of likes' do
+      expect(page).to have_content('Likes: 5')
     end
     it 'shows the posts body' do
-      expect(page).to have_content("This is my post number 1")
+      expect(page).to have_content('This is my post number 1')
     end
-    it "shows the user name of each commentor" do
-      expect(page).to have_content('Zaid:', count:6)
+    it 'shows the user name of each commentor' do
+      expect(page).to have_content('Zaid:', count: 6)
     end
-    it "shows the comment of each commentor" do
-      expect(page).to have_content('Hello Zaid!', count:6)
+    it 'shows the comment of each commentor' do
+      expect(page).to have_content('Hello Zaid!', count: 6)
     end
   end
 end
