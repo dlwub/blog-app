4ca5469cdf2054d51fc9f980011e091310ef37e8
dlwub <dlwubi@gmail.com>
2023-02-01 18:32:07 +0300

    Modify Application controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     users_url
   end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
+  end
+
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email bio photo password password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[name password])
