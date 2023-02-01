04dd47556a5543e1a426843f7452610ed48ae0ff
devMunyi <samunyi90@gmail.com>
2023-02-01 17:58:59 +0300

    Update application_controller file for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(_resource)
+    users_url
+  end
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email bio photo password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[name password])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name email bio photo password password_confirmation])
+  end
 end
